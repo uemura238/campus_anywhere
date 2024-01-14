c1a4dc92ffb1f864a16c254ff96a32bccfff9fc0
uemura238 <uemura238@icloud.com>
2024-01-14 22:23:45 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,12 @@
 set -o errexit
 
 bundle install
-yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
-bundle exec rails assets:precompile # cssはsprocketsを使っているため
-bundle exec rails db:migrate:status # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+# bundle install
+# yarn install
+# yarn build # jsファイルをesbuildでバンドルしているため
+# bundle exec rails assets:precompile # cssはsprocketsを使っているため
+# bundle exec rails db:migrate:status # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
