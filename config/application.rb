d97a6418ad01c7799cc8f751f2015cdbde3d516c
uemura238 <uemura238@icloud.com>
2024-01-14 19:51:10 +0900

    Setup for Render

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module Portfolio
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_support.cache_format_version = 7.1
+
+    config.hosts << 'https://campus-anywhere.onrender.com'
   end
 end
