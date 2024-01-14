f25ed6677bc5ba9d805501ccf895828b380a564c
uemura238 <uemura238@icloud.com>
2024-01-14 20:05:01 +0900

    修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
+
+  config.hosts << 'https://campus-anywhere.onrender.com'
 end
