93033119960c21b28ea4d7fcb9cc3812530d769b
uemura238 <uemura238@icloud.com>
2024-01-29 00:50:09 +0900

    シェルファイル設定

---
+++ bin/render-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset  #追記
+# bundle install
+# yarn install
+# yarn build # jsファイルをesbuildでバンドルしているため
+# bundle exec rails assets:precompile # cssはsprocketsを使っているため
+# bundle exec rails db:migrate:status # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
