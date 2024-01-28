c6fe7121f964540be466fe85eba0ea8cc14e5fcb
uemura238 <uemura238@icloud.com>
2024-01-29 01:05:31 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,7 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate:status
 # DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset  #追記
 # bundle install
 # yarn install
