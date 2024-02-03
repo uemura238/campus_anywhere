252a0b0ed872d2bd9453f656d957375cf466d883
uemura238 <uemura238@icloud.com>
2024-02-03 15:28:17 +0900

    ホスト許可

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Portfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << 'https://campus-anywhere.onrender.com'
+    config.hosts << 'campus-anywhere.onrender.com'
   end
 end
