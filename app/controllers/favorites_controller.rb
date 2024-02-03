48f498ac69480f31976dffc0152a7c2a3bfd2613
uemura238 <uemura238@icloud.com>
2024-02-03 18:39:44 +0900

    いいね機能を非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,11 @@
 class FavoritesController < ApplicationController
   def create
-    Favorite.create(user_id: current_user.id, micropost_id: params[:id])
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path) }
-      format.js
-    end
+    @micropost = Micropost.find(params[:id])
+    Favorite.create(user_id: current_user.id, micropost_id: @micropost.id)
   end
 
   def destroy
-    Favorite.find_by(user_id: current_user.id, micropost_id: params[:id]).destroy
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path) }
-      format.js
-    end
+    @micropost = Micropost.find(params[:id])
+    Favorite.find_by(user_id: current_user.id, micropost_id: @micropost.id).destroy
   end
 end
