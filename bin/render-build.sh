c0c7b74a2cfab04a5a90d977cbfcf03238d8ab4b
uemura238 <uemura238@icloud.com>
2024-02-03 15:13:12 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,4 @@ set -o errexit
 bundle install
 yarn install
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
-bundle exec rake db:migrate:reset
-bundle exec rails db:seed
+bundle exec rake db:migrate
