25750d56525c5661a019dc4b6103c90b053f2f3c
uemura238 <uemura238@icloud.com>
2024-02-03 13:31:28 +0900

    db接続

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
+bundle exec rake db:migrate
+bundle exec rails db:seed
